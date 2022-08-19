9e64a5c82c694ac663246de3e8cd9923bd4fa2d7
Youssef Elmoudene <youssefelmoudene09@gmail.com>
2022-08-19 22:16:47 +0100

    second commit

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { ChatComponent } from './chat/chat.component';
-import { KanbanComponent } from './kanban/kanban.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {ChatComponent} from './chat/chat.component';
+import {KanbanComponent} from './kanban/kanban.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent },
@@ -15,10 +15,7 @@ const routes: Routes = [
     { path: 'email', loadChildren: () => import('./email/email.module').then(m => m.EmailModule) },
     { path: 'pages', loadChildren: () => import('./utility/utility.module').then(m => m.UtilityModule) },
     { path: 'ui', loadChildren: () => import('./ui/ui.module').then(m => m.UIModule) },
-    { path: 'icons', loadChildren: () => import('./icons/icons.module').then(m => m.IconsModule) },
     { path: 'charts', loadChildren: () => import('./chart/chart.module').then(m => m.ChartModule) },
-    { path: 'form', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
-    { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
     { path: 'maps', loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule) },
 ];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,34 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { UiModule } from '../shared/ui/ui.module';
-import { WidgetModule } from '../shared/widget/widget.module';
+import {UiModule} from '../shared/ui/ui.module';
+import {WidgetModule} from '../shared/widget/widget.module';
 
-import { PagesRoutingModule } from './pages-routing.module';
+import {PagesRoutingModule} from './pages-routing.module';
 
-import { NgbNavModule, NgbDropdownModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgApexchartsModule } from 'ng-apexcharts';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { FullCalendarModule } from '@fullcalendar/angular';
-import { DndModule } from 'ngx-drag-drop';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { ChatComponent } from './chat/chat.component';
-import { EcommerceModule } from './ecommerce/ecommerce.module';
-import { KanbanComponent } from './kanban/kanban.component';
-import { EmailModule } from './email/email.module';
-import { UIModule } from './ui/ui.module';
-import { IconsModule } from './icons/icons.module';
-import { ChartModule } from './chart/chart.module';
-import { FormModule } from './form/form.module';
-import { TablesModule } from './tables/tables.module';
-import { MapsModule } from './maps/maps.module';
+import {NgbDropdownModule, NgbNavModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgApexchartsModule} from 'ng-apexcharts';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import {FullCalendarModule} from '@fullcalendar/angular';
+import {DndModule} from 'ngx-drag-drop';
+import {PERFECT_SCROLLBAR_CONFIG, PerfectScrollbarConfigInterface, PerfectScrollbarModule} from 'ngx-perfect-scrollbar';
+import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {ChatComponent} from './chat/chat.component';
+import {EcommerceModule} from './ecommerce/ecommerce.module';
+import {KanbanComponent} from './kanban/kanban.component';
+import {EmailModule} from './email/email.module';
+import {UIModule} from './ui/ui.module';
+import {ChartModule} from './chart/chart.module';
+import {MapsModule} from './maps/maps.module';
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true,
@@ -53,10 +47,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     DndModule,
     FullCalendarModule,
     EcommerceModule, EmailModule,
-    IconsModule,
     ChartModule,
-    FormModule,
-    TablesModule,
     MapsModule,
     LeafletModule,
     WidgetModule
